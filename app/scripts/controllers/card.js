297fd67232f7c520542ad0be73be1a75531c6ee2
Trond Skogstad <trond.skogstad@gmail.com>
2014-04-04 13:18:54 +0200

    make l1 and l2 cards

--- app/scripts/controllers/card.js
+++ app/scripts/controllers/card.js
@@ -2,8 +2,7 @@
 
 angular.module('hackdayApp')
   .controller('CardCtrl', function ($scope) {
-    $scope.l1_text = $scope.item.l1_text;
-    $scope.l2_text = $scope.item.l2_text;
+    $scope.text = $scope.item.text;
     $scope.frontUp = false;
     $scope.cardClick = function () {
       $scope.frontUp = !$scope.frontUp;
