311c1148db68a0a3428e2af5329f007a3a976046
Mikhail Dyakov <wtltl2@gmail.com>
2014-04-04 14:52:13 +0200

    Add onClick.

--- app/scripts/services/contentfactory.js
+++ app/scripts/services/contentfactory.js
@@ -13,25 +13,25 @@ angular.module('hackdayApp')
       {image_id:"381292",l1_text:"uendelig",l2_text:"infinite"}
     ];
 
-    var ReferenceCard = function (text, imageId) {
-      this.type = 'reference';
-      this.text = text;
-      this.imageId = imageId;
-      this.frontUp = false;
-    };
 
-    var LearningCard = function (text, imageId) {
-      this.type = 'learning';
+    var Card = function (type, text, imageId) {
+      this.type = type;
       this.text = text;
       this.imageId = imageId;
       this.frontUp = false;
+
+      var that = this;
+
+      this.onClick = function () {
+        console.log(that);
+      };
     };
 
     var collection = [];
 
     rawItems.forEach(function (item) {
-      var referenceCard = new ReferenceCard(item.l1_text, item.image_id);
-      var learningCard = new LearningCard(item.l2_text, item.image_id);
+      var referenceCard = new Card('reference', item.l1_text, item.image_id);
+      var learningCard = new Card('learning', item.l2_text, item.image_id);
       referenceCard.pair = learningCard;
       learningCard.pair = referenceCard;
       collection = collection.concat([referenceCard, learningCard]);
