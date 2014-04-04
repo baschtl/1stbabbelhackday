c0691bd707000f788d2441bed57d9064a71e854d
Trond Skogstad <trond.skogstad@gmail.com>
2014-04-04 12:37:52 +0200

    make cards clickable

--- app/scripts/controllers/card.js
+++ app/scripts/controllers/card.js
@@ -4,4 +4,8 @@ angular.module('hackdayApp')
   .controller('CardCtrl', function ($scope) {
     $scope.l1_text = $scope.item.l1_text;
     $scope.l2_text = $scope.item.l2_text;
+    $scope.frontUp = false;
+    $scope.cardClick = function () {
+      $scope.frontUp = !$scope.frontUp;
+    }
   });
