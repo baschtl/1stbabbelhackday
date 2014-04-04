4d8f0e49584c30f97e204c19c6e8dcafdb9079f7
Trond Skogstad <trond.skogstad@gmail.com>
2014-04-04 14:01:12 +0200

    move to content factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,16 +5,5 @@ angular.module('hackdayApp')
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
-    $scope.rawItems = contentFactory.getContent();
-    
-    $scope.l1Items = _.map($scope.rawItems, function (item) {
-      console.log({id: item.id, text: item.l1_text, image_id: item.image_id});
-      return {id: item.id, text: item.l1_text, image_id: item.image_id};
-    });
-
-    $scope.l2Items = _.map($scope.rawItems, function (item) {
-      return {id: item.id, text: item.l2_text, image_id: item.image_id};
-    });
-
-    $scope.items = _.shuffle($scope.l1Items.concat($scope.l2Items));
+    $scope.items = contentFactory.getContent();
   });
